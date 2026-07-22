b1ce6ebc713a507bd23e504ac5bfd57af31fd014
Yixuan Zhao <sophia@vl965-172-31-9-124.wireless.umass.edu>
2026-07-22 01:03:41 -0400

    Fix Swagger production configuration

--- src/main/java/com/cloudnotes/config/OpenApiConfig.java
+++ src/main/java/com/cloudnotes/config/OpenApiConfig.java
@@ -8,8 +8,10 @@ import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("!prod")
 public class OpenApiConfig {
 
     public static final String BEARER_AUTH = "bearerAuth";
